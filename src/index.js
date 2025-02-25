a09640f5ba57e8aba4a5be6e0c488e6c51405ab2
byeoon <47872200+byeoon@users.noreply.github.com>
2025-02-25 00:14:56 +0000

    feat(auth): proper auth and password hashing yay

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ require('dotenv').config()
 const express = require('express')
 const bodyparser = require('body-parser')
 const AppDataSource = require('./database')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userRoutes = require('./routes/userRoutes')
 const statsRoutes = require('./routes/statsRoutes')
 const notesRoutes = require('./routes/notesRoutes')
 
+const userRepo = AppDataSource.getRepository("User")
+
 const app = express();
 const path = require('path')
-
+/* 
 var errorHandler = require('express-error-handler'),
   handler = errorHandler({
     views: {
@@ -18,6 +22,7 @@ var errorHandler = require('express-error-handler'),
       '500': 'https://myshare.haydar.dev/500.html'
     }
   });
+*/
 
 const PORT = process.env.PORT || 3010
 const HOST = process.env.HOST || "127.0.0.1"
@@ -29,8 +34,8 @@ app.use("/api", notesRoutes)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -42,7 +47,32 @@ AppDataSource.initialize().then(() => {
     console.log("Database err:", error.message)
 })
 
-app.use( errorHandler.httpError(404) );
-app.use( errorHandler.httpError(500) );
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+        console.log("no token for some reason?");
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+  
+    jwt.verify(token, 'secret', (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      req.user = decoded;
+      next();
+    });
+  };
+
+app.get('/api/secretadminthing', verifyToken, async (req, res) => {
+    try {
+        const user = await userRepo.findOneBy({ email: req.user.email });
+        if (!user) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+        res.status(200).json({ email: user.email });
+      } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+      }
+});
   
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,37 +1,53 @@
 const express = require("express")
 const AppDataSource = require('../database')
-
+const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
 const router = express.Router();
 
 router.post("/users", async (req, res) => {
-    const { username, email, password } = req.body;
+    let { username, email, password } = req.body;
     try {
         const userRepo = AppDataSource.getRepository("User")
         const existing = await userRepo.findOneBy({ email })
         if (existing) {
             return res.status(400).json({ message : "User already exists!"})
         }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        password = hashedPassword; // This feels like unsafe shitcode.
 
         const user = userRepo.create({username, email, password});
         const newuser = await userRepo.save(user)
-
         res.status(201).json({ message: "User created.", user : newuser})
-        
-    } catch {
-        console.log(error)
+    } catch(error) {
+        console.log(error);
         res.status(500).json({message : "internal error"})
     }
 })
 
 router.post('/users/login',async(req,res) => {
-    const { username, email, password } = req.body;
-    const userRepo = AppDataSource.getRepository("User")
-    const user = await userRepo.findOne({ email: email });
-
+    const { email, password } = req.body;
+    const userRepo = AppDataSource.getRepository("User");
+    const user = await userRepo.findOneBy({ email: email });
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    // TODO: Inverse this, will cause an error if there is no email that exists. 
+    // Could just add better handling but not now
     if (!user) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+        console.log("no exist.");
+        return res.status(401).json({ error: 'Invalid credentials' });
     }
+
+    if (!passwordMatch) {
+        console.log("not the same");
+        return res.status(401).json({ error: 'Not the same password' });
+    }
+
+    console.log("is the same??");
+    const token = jwt.sign({ email: user.email }, 'secret');
+    res.status(200).json({ token });
+    console.log(token);
+  //  window.localStorage.setItem("token", token); // do not give this out to anyone!
   })
+
 
 module.exports = router;
 
